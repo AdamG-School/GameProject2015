d6650e4408510ab974618e3dd5469bd5bddc4c39
BuildTools <unconfigured@null.spigotmc.org>
2015-09-01 20:33:27 -0400

    Modified character controller

--- GameProject2015/Assets/Scripts/CharcterControls.cs
+++ GameProject2015/Assets/Scripts/CharcterControls.cs
@@ -1,23 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (CharacterController))]
 public class CharcterControls : MonoBehaviour {
 
 	public float speed;
-	public float gravity;
 
 	private CharacterController controller;
 
 	void Start () {
-		controller = gameObject.GetComponent<CharacterController>();
+
 	}
+
+	void Move
 
 	void Update () {
 		Vector3 vertMove = new Vector3 (0f, 0f,  Input.GetAxis ("Vertical") * speed);
 		Vector3 horMove = new Vector3 (Input.GetAxis ("Horizontal") * speed, 0f, 0f);
 		Vector3 movement = vertMove + horMove;
-		movement.y = movement.y + gravity;
 
 		controller.Move (Vector3.MoveTowards(movement, movement, 10f));
 	}
